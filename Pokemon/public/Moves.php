c4e5474d8242b38f90dc3a0a2685594157d67368
jayven <jayvenaltenberg@gmail.com>
2025-08-27 14:12:15 +0200

    Showing the moves from the json files based on the id wokring

---
+++ Pokemon/public/Moves.php
@@ -0,0 +1,31 @@
+<?php
+
+$jsonFile = __DIR__ . '/../data/PokemonInfo.json';
+// Read all the json content
+$jsonContent = file_get_contents($jsonFile);
+$data = json_decode($jsonContent, true);
+
+// select a specific pokemon
+$id = 58;
+$selectedPokemonData = null;
+
+foreach ($data as $pokemon) {
+    if ($id === $pokemon["id"]) {
+        $selectedPokemonData = $pokemon;
+        break;
+    };
+}
+
+// show the moves
+if (isset($selectedPokemonData)) {
+    echo $selectedPokemonData['name'];
+    print_r($selectedPokemonData['moves']);
+}
+
+
+// make a api call to show a specefic pokemon
+// what route for the api call
+
+//return what is needed
+
+// show a error if it doesnt work can use status codes
